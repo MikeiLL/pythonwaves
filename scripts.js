68fc2babc6e31a035dec4ade3ffb63bdfeb8f711
mikeill <mike@mzoo.org>
2025-08-13 09:29:05 -0500

    Start on a thwap button.

--- scripts.js
+++ scripts.js
@@ -20,10 +20,6 @@ const sineboom = (e) => {
   biquadFilter.frequency.value = 440;
   biquadFilter.gain.value = 40;
   biquadFilter.frequency.setValueAtTime(110, startTime);
-  console.log({
-    "decay / 4" : decay.value / 4,
-    "decay / 2" : decay.value / 2,
-  });
   biquadFilter.frequency.setTargetAtTime(1000, startTime, decay.value / 4);
   biquadFilter.frequency.setTargetAtTime(110, startTime, decay.value / 2);
 
@@ -43,7 +39,38 @@ const sineboom = (e) => {
   }
   cutoffTM = setTimeout(cutoff, decay.value * 2000);
 }
+const noisethwap = (e) => {
+  let btn = e.match;
+  if (cutoff) cutoff();
+  btn.classList.add("playing");
+  const gainNode = new GainNode(audioCtx);
+  // create Oscillator node
+  const oscillator = audioCtx.createOscillator();
+  const biquadFilter = new BiquadFilterNode(audioCtx);
+  let startTime = audioCtx.currentTime;
+  biquadFilter.type = "peaking";
+  biquadFilter.frequency.value = 880;
+  biquadFilter.gain.value = 40;
+  biquadFilter.frequency.setValueAtTime(440, startTime);
+  biquadFilter.frequency.setTargetAtTime(1000, startTime, decay.value / 4);
+  biquadFilter.frequency.setTargetAtTime(440, startTime, decay.value / 2);
 
+  oscillator.type = "sawtooth";
+  oscillator.frequency.setValueAtTime(523, startTime); // value in hertz
 
+  oscillator.connect(gainNode).connect(biquadFilter).connect(audioCtx.destination);
+  oscillator.start();
+  gainNode.gain.setValueAtTime(1, startTime);
+  gainNode.gain.setTargetAtTime(0, startTime, decay.value / 2);
+  cutoff = () => {
+    oscillator.stop();
+    btn.classList.remove("playing");
+    clearTimeout(cutoffTM);
+    cutoffTM = cutoff = null;
+  }
+  cutoffTM = setTimeout(cutoff, decay.value * 2000);
+}
+
+on("click", "#thwap", noisethwap);
 on("click", "#boom", sineboom);
 decay.addEventListener("change", (e) => DOM("#decayvalue").innerHTML = e.currentTarget.value);
