e9c35f0d201d35f24ac50966d08d97ab3f2ae553
mikeill <mike@mzoo.org>
2025-08-13 19:50:03 -0500

    Reverb node.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 import {choc, set_content, on, DOM} from "https://rosuav.github.io/choc/factory.js";
 const { } = choc; //autoimport
-
+import {impulseResponse} from "./assets.js";
 
 let cutoff = null, cutoffTM = null;
 const decay = document.getElementById("decay");
 const thwaplength = document.getElementById("thwaplength");
-const bandHz = document.getElementById("thwapfrequency").value;
+const bandHz = document.getElementById("thwapfrequency");
+
+// http://stackoverflow.com/questions/21797299/convert-base64-string-to-arraybuffer
+function base64ToArrayBuffer(base64) {
+  const binaryString = window.atob(base64);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
 // create web audio api context
 const audioCtx = new AudioContext();
 
@@ -49,35 +61,26 @@ const noisethwap = (e) => {
   if (cutoff) cutoff();
   btn.classList.add("playing");
   const gainNode = new GainNode(audioCtx);
-  // create Oscillator node
-  const oscillator = audioCtx.createOscillator();
-  const biquadFilter = new BiquadFilterNode(audioCtx);
   let startTime = audioCtx.currentTime;
-  biquadFilter.type = "peaking";
-  biquadFilter.frequency.value = 880;
-  biquadFilter.gain.value = 40;
-  biquadFilter.frequency.setValueAtTime(440, startTime);
-  biquadFilter.frequency.setTargetAtTime(1000, startTime, thwapfrequency.value / 4);
-  biquadFilter.frequency.setTargetAtTime(440, startTime, thwapfrequency.value / 2);
-
-  oscillator.type = "sawtooth";
-  oscillator.frequency.setValueAtTime(523, startTime); // value in hertz
-
-  oscillator.connect(gainNode).connect(biquadFilter).connect(audioCtx.destination);
-  oscillator.start();
-  gainNode.gain.setValueAtTime(0.2, startTime);
-  gainNode.gain.setTargetAtTime(0, startTime, thwapfrequency.value / 2);
-  cutoff = () => {
-    oscillator.stop();
-    btn.classList.remove("playing");
-    clearTimeout(cutoffTM);
-    cutoffTM = cutoff = null;
-  }
-  cutoffTM = setTimeout(cutoff, decay.value * 2000);
 
   /* Lift from MDN step sequencer example */
   function playNoise() {
     const bufferSize = audioCtx.sampleRate * thwaplength.value; // set the time of the note
+    const reverbNode = audioCtx.createConvolver();
+    // impulseResponse is defined in assets.js
+    // It's a base64 encoded string.
+    // Convert it to a binary array first
+    const reverbSoundArrayBuffer = base64ToArrayBuffer(impulseResponse);
+
+    audioCtx.decodeAudioData(
+      reverbSoundArrayBuffer,
+      (buffer) => {
+        reverbNode.buffer = buffer;
+      },
+      (e) => {
+        console.error("Error when decoding audio data", e.err);
+      }
+    );
 
     // Create an empty buffer
     const noiseBuffer = new AudioBuffer({
@@ -99,11 +102,15 @@ const noisethwap = (e) => {
     // Filter the output
     const bandpass = new BiquadFilterNode(audioCtx, {
       type: "bandpass",
-      frequency: bandHz,
+      frequency: bandHz.value,
     });
 
+    console.log(bandHz.value);
+
+    gainNode.gain.setValueAtTime(1, startTime);
+    gainNode.gain.setTargetAtTime(0, startTime, decay.value / 2);
     // Connect our graph
-    noise.connect(bandpass).connect(audioCtx.destination);
+    noise.connect(bandpass).connect(reverbNode).connect(gainNode).connect(audioCtx.destination);
     noise.start();
   }
   playNoise();
@@ -111,5 +118,6 @@ const noisethwap = (e) => {
 
 on("click", "#thwap", noisethwap);
 on("click", "#boom", sineboom);
-decay.addEventListener("change", (e) => DOM("#decayvalue").innerHTML = e.currentTarget.value);
-thwaplength.addEventListener("change", (e) => DOM("#thwaplengthvalue").innerHTML = e.currentTarget.value);
+decay.addEventListener("input", (e) => DOM("#decayvalue").innerHTML = e.currentTarget.value);
+thwaplength.addEventListener("input", (e) => DOM("#thwaplengthvalue").innerHTML = e.currentTarget.value);
+thwapfrequency.addEventListener("input", (e) => DOM("#thwapfrequencyvalue").innerHTML = e.currentTarget.value);
