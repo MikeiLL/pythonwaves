d39dd1a0da1d3edd612e8d2ce4d857ae9e4817cf
mikeill <mike@mzoo.org>
2025-08-15 10:11:09 -0500

    Initial sequence play needs stop button.

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ const bandHz = document.getElementById("thwapfrequency");
 const stepCount = DOM("#stepCount");
 let playing;
 let timer;
+let bpm = 60;
 
 // http://stackoverflow.com/questions/21797299/convert-base64-string-to-arraybuffer
 function base64ToArrayBuffer(base64) {
@@ -28,10 +29,10 @@ const sineboom = (e) => {
   let btn = e.match;
   if (cutoff) cutoff();
   btn.classList.add("playing");
-  playBoom(audioCtx.currentTime);
+  playBoom(audioCtx.currentTime, btn);
 }
 
-function playBoom(startTime) {
+function playBoom(startTime, btn) {
   const gainNode = new GainNode(audioCtx);
   // create Oscillator node
   const oscillator = audioCtx.createOscillator();
@@ -46,14 +47,14 @@ function playBoom(startTime) {
   oscillator.type = "sawtooth";
   oscillator.frequency.setValueAtTime(110, startTime); // value in hertz
 
-  oscillator.connect(gainNode).connect(biquadFilter).connect(audioCtx.destination);
-  oscillator.start();
+  oscillator/* .connect(gainNode).connect(biquadFilter) */.connect(audioCtx.destination);
+  oscillator.start(startTime);
   oscillator.frequency.setTargetAtTime(20, startTime, 4);
   gainNode.gain.setValueAtTime(1, startTime);
   gainNode.gain.setTargetAtTime(0, startTime, decay.value / 2);
   cutoff = () => {
     oscillator.stop();
-    btn.classList.remove("playing");
+    btn?.classList.remove("playing");
     clearTimeout(cutoffTM);
     cutoffTM = cutoff = null;
   }
@@ -115,13 +116,13 @@ function playNoise(startTime) {
   gainNode.gain.setTargetAtTime(0, startTime, decay.value / 2);
   // Connect our graph
   noise.connect(bandpass).connect(reverbNode).connect(gainNode).connect(audioCtx.destination);
-  noise.start();
+  noise.start(startTime);
 }
 
 function buildSteps() {
   document.querySelectorAll("#sequencer td").forEach(cell => cell.remove());
   for (let i = 0; i < stepCount.value; i++) {
-    rows.forEach(r => r.append(TD(INPUT({type: "checkbox"}))));
+    rows.forEach((row, r) => row.append(TD(INPUT({id: `stepaction-${r}-${i}`,type: "checkbox"}))));
   }
 }
 
@@ -135,16 +136,32 @@ const rows = document.querySelectorAll("#sequencer tr");
 
 on("click", "#togglePlay", () => {
   playing = !playing;
+  const bufferTime = 0.2;
+  let stepTime = audioCtx.currentTime; // in seconds
+  let currentStep = -1;
   function inqueue() {
+    console.log(audioCtx.currentTime);
+    const stepDuration = 60 / bpm;
+    while (stepTime < audioCtx.currentTime + bufferTime) {
+      if (++currentStep >= stepCount.value) currentStep = 0;
+      stepTime += stepDuration;
+      if (stepTime < audioCtx.currentTime) continue; // in case js gets behind
+      DOM(`#stepaction-0-${currentStep}`).checked && playBoom(stepTime);
+      DOM(`#stepaction-1-${currentStep}`).checked && playNoise(stepTime);
+    }
+
     // loop through a number of beats (step through columns)
     // if checked call corresponding function
     // loop a number of beats, know which beat we're on
     // and what time it happens
   }
-  if (playing) timer = setInterval(inqueue, 0.5);
+  if (playing) {
+    inqueue();
+    timer = setInterval(inqueue, bufferTime * 1000);
+  }
   else clearInterval(timer);
 });
 
-rows[0].append(TH("thwap"));
-rows[1].append(TH("boom"));
+rows[0].append(TH("boom"));
+rows[1].append(TH("thwap"));
 buildSteps();
